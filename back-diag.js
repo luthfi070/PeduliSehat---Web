cfcd1d0cb7df6b2591752efd5b62afe0ff1204fc
luthfi070 <luthfimusafa@gmail.com>
2020-12-27 00:23:28 +0700

    added question

--- back-diag.js
+++ back-diag.js
@@ -1,4 +1,18 @@
+var data = [];
+
 function pindahDiag() {
     window.location.href = "diagnose.html";
     console.log("halo");
+}
+
+function nextS(namaElement, tujuanPage) {
+    var radio = document.getElementsByName(namaElement);
+
+    for (var i = 0, length = radio.length; i < length; i++) {
+        if (radio[i].checked) {
+            data.push(radio[i].value);
+            window.location.href = tujuanPage + ".html";
+            break
+        }
+    }
 }
