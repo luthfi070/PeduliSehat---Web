33693a42cea604c498e9201102da47373ec5cf3a
luthfi070 <luthfimusafa@gmail.com>
2020-12-27 03:28:05 +0700

    added question

--- back-diag.js
+++ back-diag.js
@@ -11,23 +11,56 @@ function nextS(namaElement, tujuanPage) {
     for (var i = 0, length = radio.length; i < length; i++) {
         if (radio[i].checked) {
             data.push(radio[i].value);
-            window.location.href = tujuanPage + ".html";
             break
         }
     }
+
+    
+
+    var total = 5;
+    for(var i=1; i<=total; i++){
+
+        if (document.getElementById('question'+i)) {
+
+            document.getElementById('question'+i).style.display='none';
+            document.getElementById('hasil').style.display='none';
+        }
+
+    }
+
+    if(tujuanPage == "hasil"){
+        console.log(data);
+        document.getElementById('hasil').style.display='block';
+        // window.location.href = "hasil.html";
+    }
+
+        if (document.getElementById('question'+tujuanPage)) {
+
+            document.getElementById('question'+tujuanPage).style.display='block';
+            document
+        }
+}
+
+function finish() {
+    x = document.getElementsByName('radioBau');
+    console.log(x.value);
+    data.push(x.value);
+    console.log(data);
 }
 
 function send(){
-    if (!liff.isInClient()) {
-        sendAlertIfNotInClient();
-    } else {
-        liff.sendMessages([{
-            'type': 'text',
-            'text': 'asd'
-        }]).then(function() {
-            alert('Catatan Tersimpan');
-        }).catch(function(error) {
-            alert('Aduh kok error ya...', error);
-        });
-    }
+    console.log(data);
+    // if (!liff.isInClient()) {
+    //     sendAlertIfNotInClient();
+    // } else {
+        
+    //     liff.sendMessages([{
+    //         'type': 'text',
+    //         'text': 'asd'
+    //     }]).then(function() {
+    //         alert('Catatan Tersimpan');
+    //     }).catch(function(error) {
+    //         alert('Aduh kok error ya...', error);
+    //     });
+    // }
 }
